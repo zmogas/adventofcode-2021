f071246ea2e60b8e88d66447ed0fa913d890ae02
Tomas Gudauskas <zmogas@gmail.com>
2021-12-16 22:44:32 +0200

    Trying to solve Day 15 with dijkstra algorithm. Failed.

--- 15.js
+++ 15.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const day = '15';
+const day = '15d';
 
 const input = fs.readFileSync(`${day}.txt`, 'utf-8');
 const lines = input.split(/\n/);
@@ -18,6 +18,16 @@ console.log({ chiton, max });
 
 const paths = [];
 
+const valxy = (x, y, step) => {
+  const xx = parseInt(step[0]);
+  const yy = parseInt(step[1]);
+  if (x + xx > max || y + yy > max) {
+    return 99;
+  }
+  console.log(y + yy, x + xx, chiton[y + yy][x + xx])
+  return chiton[y + yy][x + xx];
+}
+
 const search = (x, y, visited, sum) => {
   const place = `${x}-${y}`;
   if (visited.includes(place)) {
@@ -34,12 +44,40 @@ const search = (x, y, visited, sum) => {
   if (x % 5 == 0 && y % 5 == 0) {
     console.log({x, y, sum});
   }
-  if (x < max) {
-    search(x + 1, y, newVisited, newSum);
+
+  // calculate next 3 steps, take lowest result. String is xyxyxy, where says how many steps to add
+  const steps = [
+    '102030',
+    '102021',
+    '101121',
+    '101112',
+    '011121',
+    '011112',
+    '010212',
+    '010203'
+  ];
+  const values = [];
+  for (let i = 0; i < steps.length; i++) {
+    console.log(steps[i].substring(0, 2), steps[i].substring(2, 4), steps[i].substring(4, 6))
+    const val = valxy(x, y, steps[i].substring(0, 2))
+      + valxy(x, y, steps[i].substring(2, 4))
+      + valxy(x, y, steps[i].substring(4, 6));
+    console.log({ val });
   }
-  if (y < max) {
-    search(x, y + 1, newVisited, newSum);
-  }
+  // if (x < max && y < max) {
+  //   const diff = chiton[y + 1][x] - chiton[y][x + 1];
+  //   if (diff > 0) {
+  //     search(x + 1, y, newVisited, newSum);
+  //   } else {
+  //     search(x, y + 1, newVisited, newSum);
+  //   }
+  // }
+  // else if (x < max) {
+  //   search(x + 1, y, newVisited, newSum);
+  // }
+  // else if (y < max) {
+  //   search(x, y + 1, newVisited, newSum);
+  // }
   // if (x > 0) {
   //   search(x - 1, y, newVisited, newSum);
   // }
@@ -77,3 +115,7 @@ real    34m35,632s
 user    34m45,924s
 sys     0m9,748s
 */
+
+
+// 776   your answer is too high
+//
---
+++ 15dijkstra.js
@@ -0,0 +1,129 @@
+const fs = require('fs');
+
+const day = '15d';
+
+const input = fs.readFileSync(`${day}.txt`, 'utf-8');
+const lines = input.split(/\n/);
+
+const chiton = [];
+
+for (let y = 0; y < lines.length; y++) {
+  const chit = lines[y].split('').map(c => parseInt(c));
+  chiton.push(chit);
+}
+
+let max = chiton.length - 1;
+
+console.log({ chiton, max });
+
+const nodes = [];
+let adjacency = {};
+const collection = [];
+
+const addNode = (node) => {
+  nodes.push(node);
+  adjacency[node] = [];
+};
+
+const addEdge = (node1, node2, weight) => {
+  adjacency[node1].push({ node: node2, weight });
+  // adjacency[node2].push({ node: node1, weight });
+};
+
+const isEmpty = () => collection.length == 0;
+
+const enqueue = (element) => {
+  if (isEmpty()) {
+    collection.push(element);
+  } else {
+    let added = false;
+    for (let i = 1; i <= collection.length; i++) {
+      if (element[1] < collection[i-1][1]) {
+        collection.splice(i-1, 0, element);
+        added = true;
+        break;
+      }
+    }
+    if (!addEdge) {
+      collection.push(element);
+    }
+  }
+};
+
+const dequeue = () => {
+  let value = collection.shift();
+  return value;
+};
+
+// addNode('0-0');
+// addNode('0-1');
+// addEdge('0-0', '0-1', chiton[1][0]);
+// addNode('1-0');
+// addEdge('0-0', '1-0', chiton[0][1]);
+// return;
+max = 4;
+
+for (let y = 0; y <= max; y++) {
+  for (let x = 0; x <= max; x++) {
+    addNode(`${x}-${y}`);
+    if (y > 0) {
+      addEdge(`${x}-${y-1}`, `${x}-${y}`, chiton[y][x]);
+      addEdge(`${x}-${y}`, `${x}-${y-1}`, chiton[y-1][x]);
+    }
+    if (x > 0) {
+      addEdge(`${x-1}-${y}`, `${x}-${y}`, chiton[y][x]);
+      addEdge(`${x}-${y}`, `${x-1}-${y}`, chiton[y][x-1]);
+    }
+  }
+}
+
+console.log(nodes, adjacency);
+
+const startNode = '0-0';
+
+let times = {};
+let backtrace = {};
+
+times[startNode] = 0;
+nodes.forEach(node => {
+  if (node != startNode) {
+    times[node] = Infinity;
+  }
+});
+
+enqueue([startNode, 0]);
+// enqueue(['0-3', 5]);
+
+while (!isEmpty()) {
+  let shortestStep = dequeue();
+  let currentNode = shortestStep[0];
+  console.log({shortestStep});
+
+  adjacency[currentNode].forEach(neighbor => {
+    console.log({neighbor});
+    let time = times[currentNode] + neighbor.weight;
+
+    if (time < times[neighbor.node] || times[neighbor.node] == Infinity) {
+      times[neighbor.node] = time;
+      backtrace[neighbor.node] = currentNode;
+      enqueue([neighbor.node, time]);
+    }
+  });
+}
+
+console.log(times);
+// console.log(backtrace);
+console.log();
+console.log('Time table');
+for (let y = 0; y <= max; y++) {
+  let line = '';
+  for (let x = 0; x <= max; x++) {
+    let t = times[`${x}-${y}`];
+    if (t < Infinity) {
+      line += (t + '')[0];
+    } else {
+      line += '.';
+    }
+  }
+  console.log(line);
+}
